e7fda1e0d907df9662c059fe64713c2fc7a79c26
RicardoAcostaArambula <acostaricardo463@gmail.com>
2024-09-22 16:07:35 -0600

    adds general structure for all cases and script file for running copilation faster

--- head.c
+++ head.c
@@ -1,7 +1,7 @@
 
 /**
  * Happy and Edge cases:
- * Case 1: No option & on file argument
+ * Case 1: No option & no file argument
     - When running without any option nor file argument, the head command only 
     copies the first 10 lines of the input it receives on standard input (stdin)
     and disregard any other lines seen on input
@@ -18,7 +18,7 @@
         - if the ammount is negative, head do nothing either 
         - you are supposed to perform the string-to-integer conversion with code written on your
         own; not usign atoi or strtol which will be level-3 operations 
-filename & num optionals
+ * Case 4:  filename & num optionals || num optionals & filename
     - they can be combine in any resonable order
 * Notes
     1. filename & num optionals
@@ -34,24 +34,74 @@ filename & num optionals
 */
 
 #include <stdio.h>
-/*checks if c is a digit */
+/*functions declarations */
 int isDigit(char c);
+int my_atoi(char s[]);
+int isSame(char value[], char target[]);
 
 int main(int argc, char **argv){
+    /*No file and no option*/
+    int numberOfLines;
+
+    if (argc == 1){
+        numberOfLines = 10;
+    } else if (argc == 2){
+        /*check that second argument is a file name*/
+    }
+    else if (argc == 3){
+        /* Checking for case = ./head -n 10 */
+        char n[] = "-n";
+        if (isSame(argv[1], n)){
+            numberOfLines = my_atoi(argv[2]);
+        } else {
+            /*rise error for invalid input*/
+        }
+
+    } else if (argc == 4){
+        /* check for case: 1. filename -n # 2. -n # filename */
+        char n[] = "-n";
+        /* case when -n goes the second argument */
+        if (isSame(argv[1], n)){
+            numberOfLines = my_atoi(argv[2]);
+        } else if (isSame(argv[2], n)){
+        /* case when -n goes the third index argument */
+            numberOfLines = my_atoi(argv[3]);
+        }
+    }
     return 0;
 }
-/*atoi: convert s to integer*/
-int atoi(char s[]){
+
+
+
+
+
+/*my_atoi: convert s to integer*/
+int my_atoi(char s[]){
     int i, n, sign;
     i=0;
     sign = (s[i]== '-')? -1 : 1;
     if (s[i]=='+' || s[i]=='-')
         i++;
-    for (n=0; isDigit(s[i]); i++)
+
+    for (n=0; s[i]!='\0'; i++){
+        if (!isDigit(s[i])){
+            /*rise error*/
+
+        }
         n = 10 * n + (s[i] - '0');
+    }
     return sign * n;
 }
-
+/*checks if c is a digit */
 int isDigit(char c){
     return '0' <= c  && c <='9';
+}
+
+int isSame(char value[], char target[]){
+    int i, same;
+    i = 0;
+    while ((value[i] != '\0' && target[i] != '\0') && (value[i] == target[i]))
+        i++;
+    same = (value[i] =='\0' && target[i]=='\0');
+    return same;
 }
---
+++ run_head.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+gcc -Wall -O3 -o head head.c
