77f421aa3d199b32602eaf502c16dbdc2ef8fa12
RicardoAcostaArambula <acostaricardo463@gmail.com>
2024-09-25 13:20:00 -0600

    adds general structure of tail

--- tail.c
+++ tail.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 
 int main(int argc, char **argv){
+
+    if (argc == 1){
+        /*Case when tail is call without -n optional nor file*/
+
+    } else if (argc == 2){
+        /*Case when tail is call with a file*/
+
+    } else if (argc == 3){
+        /*case when tail is call with an optional -n and a number of lines*/
+        /*Ex. tail -n 10*/
+    } else if (argc ==4){
+        /*case when tail is call with a file and optional in reasonable order*/
+
+    }
     return 0;
 }
