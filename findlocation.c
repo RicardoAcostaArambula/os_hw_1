a17b992ea17d149afa61b5938dc3ff6dd184eca9
RicardoAcostaArambula <acostaricardo463@gmail.com>
2024-09-29 18:15:34 -0600

    adds comments to findlocation

--- findlocation.c
+++ findlocation.c
@@ -11,6 +11,7 @@ ssize_t my_write(int fd, const char *buf, size_t count);
 int my_strlen(char c[]);
 int get_number(char* line);
 void get_location(char *line, char *current_location);
+int display_error_message(const char *buf, size_t count);
 /**
  * Missing items: handle the case when lseek fails on the second argument (file)
 */
@@ -25,32 +26,35 @@ int main(int argc, char **argv){
     if (argc == 1){
         /*tool must fail and display a usage message on standard error*/
         message = "Error: not enough arguments\nUsage: ./findlocation  <number> or ./findlocation  <number> <filename>\n";
-        my_write(2, message, my_strlen(message));
+        length = my_strlen(message);
+        display_error_message(message, length);
         rc = 1;
+        found = 1;
         goto end;
     } else if (argc == 2){
         /*check that the argument is a valid number and if so we have the number in the target variable*/
         if ((target = my_atoi(argv[1]))<1){
             message = "Error: invalid number, please enter only digits.\n";
-            my_write(2, message, my_strlen(message));
+            length = my_strlen(message);
+            display_error_message(message, length);
             rc = 1;
+            found = 1;
             goto end;
         }
         fd = open(filename, O_RDONLY);
         if (fd < ((size_t) 0)){
             message = "Error: could not open file\n";
             length = my_strlen(message);
-            my_write(1, message, length);
+            display_error_message(message, length);
             rc = 1;
             goto end;
         }
-        // /**File is open now use mmap()*/
-        // struct stat struct_block;
+        /**File is open now it is using mmap()*/
         off_t file_size = lseek(fd, 0, SEEK_END);
         if (file_size < ((size_t) 0)){
             message = "Error: could not get the file size\n";
             length = my_strlen(message);
-            my_write(2, message, length);
+            display_error_message(message, length);
             rc = 1;
             goto end;
         }
@@ -58,11 +62,13 @@ int main(int argc, char **argv){
         if (mappedFile == MAP_FAILED){
             message = "Error: could not store the file using mmap\n";
             length = my_strlen(message);
-            my_write(2, message, length);
+            display_error_message(message, length);
             close(fd);
             rc = 1;
             goto end;
         }
+
+        /**performs binary search*/
         int low, high, middle, current_number;
         char current_location[25];
         low = 0; high = (file_size-ROW_SIZE);
@@ -83,10 +89,11 @@ int main(int argc, char **argv){
                 low = (middle + ROW_SIZE);
             }
         }
+        /**frees memory used for the mapping*/
         if (munmap(mappedFile, file_size) < ((size_t) 0)){
             message = "Error: an error occurr while using munmap\n";
             length = my_strlen(message);
-            my_write(2, message, length);
+            display_error_message(message, length);
             close(fd);
             rc = 1;
             goto end;
@@ -98,7 +105,8 @@ int main(int argc, char **argv){
         */
         if ((target = my_atoi(argv[1]))<1){
             message = "Error: invalid number, please enter only digits.\n";
-            my_write(2, message, my_strlen(message));
+            length = my_strlen(message);
+            display_error_message(message, length);
             rc = 1;
             goto end;
         }
@@ -107,16 +115,16 @@ int main(int argc, char **argv){
         if (fd < ((size_t) 0)){
             message = "Error: could not open file\n";
             length = my_strlen(message);
-            my_write(1, message, length);
+            display_error_message(message, length);
             rc = 1;
             goto end;
         }
-
+        /**Gets the file size by using SEEL_END*/
         off_t file_size = lseek(fd, 0, SEEK_END);
         if (file_size < ((size_t) 0)){
             message = "Error: could not get the file size\n";
             length = my_strlen(message);
-            my_write(2, message, length);
+            display_error_message(message, length);
             rc = 1;
             goto end;
         }
@@ -124,12 +132,12 @@ int main(int argc, char **argv){
         if (mappedFile == MAP_FAILED){
             message = "Error: could not store the file using mmap\n";
             length = my_strlen(message);
-            my_write(2, message, length);
+            display_error_message(message, length);
             close(fd);
             rc = 1;
             goto end;
         }
-
+        /**performs binary search*/
         int low, high, middle, current_number;
         char current_location[25];
         low = 0; high = (file_size-ROW_SIZE);
@@ -153,7 +161,7 @@ int main(int argc, char **argv){
         if (munmap(mappedFile, file_size) < ((size_t) 0)){
             message = "Error: an error occurr while using munmap\n";
             length = my_strlen(message);
-            my_write(2, message, length);
+            display_error_message(message, length);
             close(fd);
             rc = 1;
             goto end;
@@ -164,7 +172,7 @@ end:
     if (!found){
         message = "Number was not found\n";
         length = my_strlen(message);
-        my_write(2, message, length);
+        my_write(1, message, length);
     }
     return rc;
 }
@@ -216,13 +224,14 @@ ssize_t my_write(int fd, const char *buf, size_t count){
     }
     return total_written;
 } 
+/**gets the size of the string*/
 int my_strlen(char c[]){
     int i;
     for(i=0;c[i]!='\0';i++)
         ;
     return i;
 }
-
+/*gets the number from the file*/
 int get_number(char *line){
     int i, number;
     number = 0;
@@ -233,9 +242,8 @@ int get_number(char *line){
     }
     return number;
 }
+/*gets teh location form the file*/
 void get_location(char *line, char *current_location){
-    //line = 201200Jersey City NJ           
-    // current_location;
     int i, j;
     for (i = 6, j = 0; i < 31; i++, j++){
         if (line[i] == ' ' && line[i+1] == ' '){
@@ -246,4 +254,28 @@ void get_location(char *line, char *current_location){
         current_location[j] = line[i];
     }
     return;
+}
+/**displays error message*/
+int display_error_message(const char *buf, size_t count){
+    int fd, length;
+    fd = 2;
+    size_t total_written = 0;
+    ssize_t bytes_written;
+    char *message;
+    if (count == ((size_t) 0)) return (ssize_t) 0;
+    while (total_written < count){
+        bytes_written = write(fd, buf + total_written, count - total_written);
+        if (bytes_written < ((size_t) 0)) {
+            message = "An error ocurred while writing";
+            length = my_strlen(message);
+            my_write(2, message, length);
+            return bytes_written;
+        }
+        if (bytes_written == ((size_t) 0)){
+            /*Nothing to be written*/
+            return total_written;
+        }
+        total_written+=bytes_written;
+    }
+    return total_written;
 }
